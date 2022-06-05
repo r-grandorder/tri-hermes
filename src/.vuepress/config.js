8960fb576eae9ad59dbfa8ba756ee7d16592907c
squaresmile <squaresmile@protonmail.com>
2022-06-05 11:38:59 +0700

    Upgraded to vuepress 2

--- src/.vuepress/config.js
+++ src/.vuepress/config.js
@@ -1,23 +1,12 @@
-import { defineUserConfig } from "vuepress";
-import type { DefaultThemeOptions } from "vuepress";
-import { sitemap } from "vuepress-plugin-sitemap2";
-import { seo } from "vuepress-plugin-seo2";
+const { defaultTheme } = require("vuepress");
+// const { seoPlugin } = require("vuepress-plugin-seo2");
+// const { sitemapPlugin } = require("vuepress-plugin-sitemap2");
 
-export default defineUserConfig<DefaultThemeOptions>({
-    /**
-     * Ref：https://v1.vuepress.vuejs.org/config/#title
-     */
+module.exports = {
+    base: "/rgo-wiki/",
+    lang: "en-US",
     title: "FGO Wiki",
-    /**
-     * Ref：https://v1.vuepress.vuejs.org/config/#description
-     */
-    description: "r/grandorder wiki",
-
-    /**
-     * Extra tags to be injected to the page HTML `<head>`
-     *
-     * ref：https://v1.vuepress.vuejs.org/config/#head
-     */
+    description: "/r/grandorder wiki",
     head: [
         ["meta", { name: "theme-color", content: "#3eaf7c" }],
         ["meta", { name: "apple-mobile-web-app-capable", content: "yes" }],
@@ -26,13 +15,7 @@ export default defineUserConfig<DefaultThemeOptions>({
             { name: "apple-mobile-web-app-status-bar-style", content: "black" },
         ],
     ],
-
-    /**
-     * Theme configuration, here is the default theme configuration for VuePress.
-     *
-     * ref：https://v1.vuepress.vuejs.org/theme/default-theme-config.html
-     */
-    themeConfig: {
+    theme: defaultTheme({
         smoothScroll: true,
         repo: "https://github.com/squaresmile/rgo-wiki",
         editLinks: true,
@@ -129,74 +112,14 @@ export default defineUserConfig<DefaultThemeOptions>({
                 },
             ],
         },
-    },
-
-    /**
-     * Apply plugins，ref：https://v1.vuepress.vuejs.org/zh/plugin/
-     */
+    }),
     plugins: [
-        "@vuepress/plugin-back-to-top",
-        "@vuepress/plugin-medium-zoom",
-        seo({
-            hostname: "https://squaresmile.github.io/rgo-wiki",
-            isArticle: () => false,
-            customHead: (head) => {
-                for (let index = head.length - 1; index >= 0; index--) {
-                    if (head[index][1]?.["name"] == "twitter:card") {
-                        head.splice(index, 1);
-                    }
-                }
-
-                head.push(
-                    [
-                        "meta",
-                        {
-                            property: "og:image",
-                            content: "https://docs.chaldea.center/logo.png",
-                        },
-                    ],
-                    [
-                        "meta",
-                        {
-                            property: "og:image:type",
-                            content: "image/png",
-                        },
-                    ],
-                    [
-                        "meta",
-                        {
-                            property: "og:image:width",
-                            content: "64",
-                        },
-                    ],
-                    [
-                        "meta",
-                        {
-                            property: "og:image:height",
-                            content: "64",
-                        },
-                    ],
-                    [
-                        "meta",
-                        {
-                            property: "og:image:alt",
-                            content: "/r/grandorder icon",
-                        },
-                    ],
-                    [
-                        "meta",
-                        {
-                            property: "og:description",
-                            content:
-                                "/r/grandorder wiki",
-                        },
-                    ]
-                );
-            },
-        }),
-        sitemap({
-            hostname: "https://squaresmile.github.io/rgo-wiki",
-            changefreq: "monthly",
-        }),
+        // seoPlugin({
+        //     hostname: "https://squaresmile.github.io/rgo-wiki",
+        // }),
+        // sitemapPlugin({
+        //     hostname: "https://squaresmile.github.io/rgo-wiki",
+        //     changefreq: "monthly",
+        // }),
     ],
-});
+};
