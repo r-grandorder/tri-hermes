9cbf224457da100d0f483b000363294efb1fec8f
squaresmile <squaresmile@protonmail.com>
2022-06-07 06:05:06 +0000

    Added ogp tags and sitemap

--- src/.vuepress/config.js
+++ src/.vuepress/config.js
@@ -1,6 +1,6 @@
 const { defaultTheme } = require("vuepress");
-// const { seoPlugin } = require("vuepress-plugin-seo2");
-// const { sitemapPlugin } = require("vuepress-plugin-sitemap2");
+const { seoPlugin } = require("vuepress-plugin-seo2");
+const { sitemapPlugin } = require("vuepress-plugin-sitemap2");
 const { googleAnalyticsPlugin } = require('@vuepress/plugin-google-analytics');
 const { searchPlugin } = require('@vuepress/plugin-search')
 
@@ -16,7 +16,6 @@ module.exports = {
             "meta",
             { name: "apple-mobile-web-app-status-bar-style", content: "black" },
         ],
-        ["meta", { property: 'og:image', href: 'https://i.imgur.com/ZsRygrA.png' }]
     ],
     theme: defaultTheme({
         smoothScroll: true,
@@ -72,13 +71,14 @@ module.exports = {
         sidebar: 'auto'
     }),
     plugins: [
-        // seoPlugin({
-        //     hostname: "https://squaresmile.github.io/rgo-wiki",
-        // }),
-        // sitemapPlugin({
-        //     hostname: "https://squaresmile.github.io/rgo-wiki",
-        //     changefreq: "monthly",
-        // }),
+        seoPlugin({
+            hostname: "https://r-grandorder.github.io",
+            fallBackImage: "https://i.imgur.com/ZsRygrA.png",
+        }),
+        sitemapPlugin({
+            hostname: "https://r-grandorder.github.io",
+            changefreq: "monthly",
+        }),
         googleAnalyticsPlugin({
             id: 'G-19VNBHS02E'
         }),
